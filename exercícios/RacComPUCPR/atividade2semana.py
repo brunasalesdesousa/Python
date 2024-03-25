eaf37af27853e740ec9dacdbf58542140015ddb4
Bruna Sales <144288264+brunasalesdesousa@users.noreply.github.com>
2024-03-25 19:10:34 -0300

    atividade 2 semana facul

---
+++ exercícios/RacComPUCPR/atividade2semana.py
@@ -0,0 +1,49 @@
+
+#Mostrando o Menu Principal
+print('***Bem-Vindo ao menu!***')
+print('1. Estudantes')
+print('2. Professores')
+print('3. Disciplinas')
+print('4. Turmas')
+print('5. Mattrículas')
+print('0. Sair')
+#Coletar a opção escolhida pelo usuário
+opcao = input('Digite uma das opções:')
+
+if opcao == '1':
+        print ('Você escolheu a opção  {}'.format(opcao))
+elif opcao == '2':
+        print ('Você escolheu a opção  {}'.format(opcao))
+elif opcao == '3':
+        print ('Você escolheu a opção  {}'.format(opcao))
+elif opcao == '4':
+        print ('Você escolheu a opção  {}'.format(opcao))
+elif opcao == '5':
+        print ('Você escolheu a opção  {}'.format(opcao))
+elif opcao == '0':
+        print ('Você escolheu a opção  {}'.format(opcao))
+else:
+    print('Você digitou uma opção INVÁLIDA')
+        
+#Mostrando o segundo menu
+print('***Menu de operações - Segunda Opção***')
+print('1. Listar')
+print('2. Criar')
+print('3. Atualizar')
+print('4. Excluir')
+print('5. Voltar ao menu')
+#Coletar a opção do segundo menu
+opcao_secundaria = input('Digite uma opção valida:')
+if opcao_secundaria == '1':
+        print ('Você escolheu a opção  {}'.format(opcao_secundaria))
+elif opcao_secundaria == '2':
+        print ('Você escolheu a opção  {}'.format(opcao_secundaria))
+elif opcao_secundaria == '3':
+        print ('Você escolheu a opção  {}'.format(opcao_secundaria))
+elif opcao_secundaria == '4':
+        print ('Você escolheu a opção  {}'.format(opcao_secundaria))
+elif opcao_secundaria == '5':
+        print ('Você escolheu a opção  {}'.format(opcao_secundaria))
+else:
+    print('Você digitou uma opção INVÁLIDA')
+
