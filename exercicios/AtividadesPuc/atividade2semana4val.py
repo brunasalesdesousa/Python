a3b61024c4e90de4826d9b27af467d446ddf3129
Bruna Sales <144288264+brunasalesdesousa@users.noreply.github.com>
2024-04-21 18:34:58 -0300

    tuplas e dicionários

--- exercicios/AtividadesPuc/atividade2semana4val.py
+++ exercicios/AtividadesPuc/atividade2semana4val.py
@@ -1,5 +1,5 @@
 
-estudantes = []
+lista_estudantes = []
 
 
 while True:
@@ -36,19 +36,49 @@ while True:
 
     if opcao_secundaria == '1':
        print ('Você escolheu a opção INCLUIR nº {}'.format(opcao_secundaria))
-       nome = input('Digite seu nome completo:')
-       estudante = [nome]
-       estudantes.append(estudante)
+       codigo = int(input('Digite o código do estudante: '))
+       nome = input('Digite seu nome do estudante completo:')
+       cpf = input('Digite o CPF do estudante: ')
+
+       dicionario_estudante = {}
+       dicionario_estudante['cod_estudante'] = codigo
+       dicionario_estudante['nome_estudante'] = nome
+       dicionario_estudante['cpf'] = cpf
+       lista_estudantes.append(dicionario_estudante)
     elif opcao_secundaria =='2':   
-       print (estudantes) 
+       print (lista_estudantes) 
        
-       if len(estudantes) == 0:
+       if len(lista_estudantes) == 0:
         print ('Não há estudantes cadastrados')
        else:
-        for i in estudantes:  
-         print (i) 
-    elif opcao_secundaria =='3'or opcao_secundaria =='4':
-       print('Opção escolhida EM DESENVOLVIMENTO')
+        for estudante in lista_estudantes:  
+         print (estudante) 
+    elif opcao_secundaria =='3':
+       print('Você escolheu atualizar estudante da lista')
+       codigo_para_atualizar = int(input('Digite o código do estudante que deseja atualizar: '))
+       estudante_atualizado = None
+       for dicionario_estudante in lista_estudantes:
+           if dicionario_estudante['cod_estudante'] == estudante_atualizado:
+               estudante_atualizado = dicionario_estudante
+               break
+       if estudante_atualizado is None:
+           print('Estudante {} não encontrado'.format(codigo_para_atualizar))
+       else:
+           estudante_atualizado['cod_estudante'] = int(input('Digite o novo código do estudante: '))
+           estudante_atualizado['nome_estudante'] = input('Digite o novo nome do estudante: ')
+           estudante_atualizado['cpf'] = input('Digite o novo cpf do estudante: ')
+
+           
+
+    elif opcao_secundaria =='4':
+        print('Você escolheu excluir estudante da lista')   
+        codigo_para_excluir = int(input('Digite o código do estudante que deseja excluir: '))
+        estudante_removido = None
+        for dicionario_estudante in lista_estudantes:
+            if dicionario_estudante['cod_estudante'] == codigo_para_excluir:
+                estudante_removido = dicionario_estudante
+                break
+        lista_estudantes.remove(estudante_removido)   
     elif opcao_secundaria =='5': 
        print('Você escolheu voltar ao Menu Principal')
        break
